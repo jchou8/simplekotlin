9318e32db406253dd3c465a2574e0e45468a8c9f
jchou8 <joseph98c@gmail.com>
2019-04-09 19:36:27 -0700

    person and money classes

--- main.kts
+++ main.kts
@@ -22,14 +22,46 @@ fun add(a: Int, b: Int) = a + b
 fun sub(a: Int, b: Int) = a - b
 
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
-fun mathOp(a: Int, b: Int, f: (Int, Int) -> Int): Int {
-    return f(a, b)
-}
+fun mathOp(a: Int, b: Int, f: (Int, Int) -> Int) = f(a, b)
 
 // write a class "Person" with first name, last name and age
+class Person(var firstName: String, var lastName: String, var age: Int) {
+    val debugString
+        get() = "[Person firstName:${firstName} lastName:${lastName} age:${age}]"
+    
+    override fun hashCode() = this.debugString.hashCode()
 
+    fun equals(other: Person) = this.hashCode() == other.hashCode()
+}
 
 // write a class "Money"
+class Money(var amount: Int, var currency: String) {
+    fun convert(otherCur: String): Money {
+        val mult: Double
+        if (this.currency === "USD") {
+            mult = when(otherCur) {
+                "EUR" -> 1.5
+                "GBP" -> 0.5
+                "CAN" -> 1.25
+                else ->  1.0
+            }
+            return Money((this.amount.toDouble() * mult).toInt(), otherCur)
+        } else {
+            mult = when(this.currency) {
+                "EUR" -> 2/3.0
+                "GBP" -> 2.0
+                "CAN" -> 0.8
+                else -> 1.0
+            }
+
+            return Money((this.amount.toDouble() * mult).toInt(), "USD").convert(otherCur)
+        }
+    }
+    
+    operator fun plus(other: Money): Money {
+        return Money(this.amount + other.convert(this.currency).amount, this.currency)
+    }
+}
 
 
 // ============ DO NOT EDIT BELOW THIS LINE =============
@@ -49,7 +81,6 @@ for ((k,v) in when_tests) {
     print(if (whenFn(k) == v) "." else "!")
 }
 println("")
-
 
 print("Add tests: ")
 val add_tests = listOf(
@@ -82,7 +113,6 @@ print(if (mathOp(2, 2, ::sub ) == 0) "." else "!")
 print(if (mathOp(2, 2, { l,r -> l*r} ) == 4) "." else "!")
 println("")
 
-/* 
 print("Person tests: ")
 val p1 = Person("Ted", "Neward", 47)
 print(if (p1.firstName == "Ted") "." else "!")
@@ -117,4 +147,3 @@ for ( (pair, result) in moneyadd_tests) {
               (pair.first + pair.second).currency == result.currency) "." else "!")
 }
 println("")
-*/
